ab836247816102f0c4695c621228ebf2cca75e8d
Chris Moore <git@chrismoore.dev>
2022-05-17 14:27:56 -0500

    add pseudocode for main loop

--- game.py
+++ game.py
@@ -1,6 +1,18 @@
+#!/usr/bin/python3
 
 def main():
     print("Hello World!")
+
+def guess_value(val, secs):
+    # every N time period for `secs` seconds
+        # poll user position
+        # set based on closeness to `val`...
+            # piezo pitch
+            # display density
+        # if within 1cm of `val`
+            # declare user winner
+            # move to next phase
+
 
 if __name__ == "__main__":
     main()
